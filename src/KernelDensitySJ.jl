1c5bd46e16aa546f2b7b05c35a96c28e3535dea8
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2020-05-27 16:22:56 +0200

    Work on new algorithm which computes gradually tighter bounds

--- src/KernelDensitySJ.jl
+++ src/KernelDensitySJ.jl
@@ -1,6 +1,7 @@
 module KernelDensitySJ
 
 using Statistics
+using DataStructures
 using Roots
 
 export bwsj
--- src/sj.jl
+++ src/sj.jl
@@ -41,6 +41,152 @@ end
 
 
 
+#---------------------------------------------------------------------------------------------------
+# New algorithm test
+
+
+struct Block
+	i1::Int
+	i2::Int
+	j1::Int
+	j2::Int
+end
+
+struct Bounds
+	lb::Float64
+	ub::Float64
+end
+function Base.isless(b1::Bounds,b2::Bounds)
+	r1 = b1.ub-b1.lb
+	r2 = b2.ub-b2.lb
+	r1!=r2 ? isless(r2,r1) : isless(b2.lb,b1.lb) # NB: this order to sort by max instead of mean
+end
+isexact(b::Bounds) = b.lb===b.ub
+
+
+function bounds(::Type{T}, block::Block, α, X) where T
+	i1,i2,j1,j2 = block.i1,block.i2,block.j1,block.j2
+
+	if (i2-i1)<=5 || (j2-j1)<=5 # fallback - loop over the data points
+		# @info "Fallback for block $block"
+		s = zero(T)
+		if i1==j1
+			@inbounds for i in i1:i2
+				for j in max(i+1,j1):j2 # upper triangular part
+					s += ϕ4((X[i]-X[j])/α)
+				end
+			end
+		else
+			@inbounds for i in i1:i2
+				for j in j1:j2
+					s += ϕ4((X[i]-X[j])/α)
+				end
+			end
+		end
+		Bounds(s,s) # exact bounds
+	elseif i1==j1 # block on the diagonal
+		@assert i2==j2
+		npoints = div((i2-i1+1)*(i2-i1),2)
+		lb,ub = npoints.*(-0.7399861849949221, 1.1968268412042982) # TODO: improve this
+		Bounds(lb,ub)
+	else
+		npoints = (i2-i1+1)*(j2-j1+1)
+		lb,ub = npoints.*ϕ4extrema((X[j1]-X[i2])/α, (X[j2]-X[i1])/α)
+		Bounds(lb,ub)
+	end
+end
+
+
+
+# Let s = ∑ᵢ∑ⱼϕⁱᵛ(α⁻¹(Xᵢ-Xⱼ)).
+# returns sign(s-C)
+function ssign(::Type{T},α,X,C)::Int where T
+	n = length(X)
+	C -= n*1.1968268412042982 # n*ϕ4(0) - get rid of the diagonal entries
+	C/=2 # because of symmetry, we can sum over j>i (upper triangular part), effectively halving the sum
+
+	root = Block(1,n,1,n)
+	queue = PriorityQueue{Block,Bounds}()
+	globalBounds = bounds(T, root, α, X)
+	enqueue!(queue,root,globalBounds)
+
+	while true
+		globalBounds.lb > C && return 1
+		globalBounds.ub < C && return -1
+		isempty(queue) && break
+		block,blockBounds = dequeue_pair!(queue)
+
+		# remove existing bounds
+		globalBounds = Bounds(globalBounds.lb-blockBounds.lb, globalBounds.ub-blockBounds.ub)
+
+		# create child blocks
+		midI = div(block.i1+block.i2,2)
+		midJ = div(block.j1+block.j2,2)
+
+		child11 = Block(block.i1, midI, block.j1, midJ)
+		childBounds11 = bounds(T, child11, α, X)
+		globalBounds = Bounds(globalBounds.lb+childBounds11.lb, globalBounds.ub+childBounds11.ub)
+		isexact(childBounds11) || enqueue!(queue, child11, childBounds11)
+
+		child12 = Block(block.i1, midI, midJ+1, block.j2)
+		childBounds12 = bounds(T, child12, α, X)
+		globalBounds = Bounds(globalBounds.lb+childBounds12.lb, globalBounds.ub+childBounds12.ub)
+		isexact(childBounds12) || enqueue!(queue, child12, childBounds12)
+
+		if block.j1>block.i1 # only care about upper triangular part
+			child21 = Block(midI+1, block.i2, block.j1, midJ)
+			childBounds21 = bounds(T, child21, α, X)
+			globalBounds = Bounds(globalBounds.lb+childBounds21.lb, globalBounds.ub+childBounds21.ub)
+			isexact(childBounds21) || enqueue!(queue, child21, childBounds21)
+		end
+
+		child22 = Block(midI+1, block.i2, midJ+1, block.j2)
+		childBounds22 = bounds(T, child22, α, X)
+		globalBounds = Bounds(globalBounds.lb+childBounds22.lb, globalBounds.ub+childBounds22.ub)
+		isexact(childBounds22) || enqueue!(queue, child22, childBounds22)
+	end
+
+	@warn "Numerical issues for upper/lower bounds, ub=$ub, lb=$lb, C=$C."
+	return 0
+end
+
+
+
+
+# Computes the sign of the objective function by gradually approximating using lower/upper bounds
+function objectivesign(h, X::AbstractArray{T}, α2Constant) where T
+	n = length(X)
+	α2 = α2Constant*h^(5/7) # 1.357[SD(a)/TD(b)]^(1/7) * h^(5/7)
+
+	C = (n-1)*α2Constant^5*h^(-10/7)/(2*√π)
+	ssign(promote_type(Float64,T), α2, X, C)
+end
+
+
+function _bwsj_bounded(X; rtol=0.1)
+	n = length(X)
+	q25,q75 = quantile(X,(0.25,0.75))
+	λ = min(q75-q25, std(X)*1.349) # Givivng the same scale estimate as e.g. the KernSmooth R package.
+	a = 0.920λ*n^(-1/7)
+	b = 0.912λ*n^(-1/9)
+	α2Constant = 1.357*(SD(a,X)/TD(b,X))^(1/7)
+
+	# Decide if we need to deal with bounds better
+	lower = λ*1e-3
+	upper = λ*1e3
+
+	find_zero(h->objectivesign(h,X,α2Constant), (lower,upper), Bisection(), xrtol=rtol)
+end
+
+
+bwsj_bounded(X; kwargs...) = _bwsj_bounded(issorted(X) ? X : sort(X); kwargs...)
+
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+
 function SDrec(::Type{T},α,X,i1,i2,j1,j2)::T where T
 	j1<i1 && return zero(T) # early out, skip lower triangular part
 
@@ -102,11 +248,8 @@ function SDreference(α, X::AbstractArray{T}) where T
 	C = 1/(n*(n-1)*α^5)
 	Y = X./α
 	s = zero(T)
-	# for yi in Y, yj in Y # TODO: fix inefficient summation.
-	# 	s += ϕ4(yi-yj)
-	# end
 	@inbounds for i=1:n
-		for j=i:n
+		for j=i+1:n
 			s += 2*ϕ4(Y[i]-Y[j])
 		end
 	end
@@ -120,9 +263,12 @@ function TD(b, X::AbstractArray{T}) where T
 	C = -1/(n*(n-1)*b^7)
 	Y = X./b
 	s = zero(T)
-	for yi in Y, yj in Y # TODO: fix inefficient summation.
-		s += ϕ6(yi-yj)
+	@inbounds for i=1:n
+		for j=i+1:n
+			s += 2*ϕ6(Y[i]-Y[j])
+		end
 	end
+	s += n*ϕ6(0)
 	C * s
 end
 
@@ -133,8 +279,9 @@ function objective(h, X, α2Constant)
 	# σ⁴ₖ = 1
 	n = length(X)
 	α2 = α2Constant*h^(5/7) # 1.357[SD(a)/TD(b)]^(1/7) * h^(5/7)
-	denom = (n*2*√π) * SD(α2,X)
-	denom^(-1/5) - h
+	# denom = (n*2*√π) * SD(α2,X)
+	# denom^(-1/5) - h
+	1/(2*√(π)*n*h^5) - SD(α2,X)
 end
 
 
