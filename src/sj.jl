32d406378e2e0b3de086b4a568b4fbd2bc12ac05
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2020-06-08 11:33:40 +0200

    More cleanup

--- src/sj.jl
+++ src/sj.jl
@@ -161,16 +161,15 @@ depth(t::SumTree) = length(t.intervalSums)
 
 
 #---------------------------------------------------------------------------------------------------
-# New algorithm v2
 
-struct Block2
+struct Block
 	depth::Int
 	k1::Int
 	k2::Int
 	lb::Float64
 	ub::Float64
 end
-Base.isless(b1::Block2,b2::Block2) = isless(b1.ub-b1.lb, b2.ub-b2.lb)
+Base.isless(b1::Block,b2::Block) = isless(b1.ub-b1.lb, b2.ub-b2.lb)
 
 
 function ϕsum(::Val{N}, ::Type{T}, k1, k2, α, X, leafSize) where {N,T}
@@ -200,7 +199,7 @@ function ϕsum(::Val{N}, ::Type{T}, k1, k2, α, X, leafSize) where {N,T}
 end
 
 
-function bounds2(::Val{N}, ::Type{T}, d, k1, k2, α, X, tree::SumTree)::Tuple{T,T} where {N,T}
+function bounds(::Val{N}, ::Type{T}, d, k1, k2, α, X, tree::SumTree)::Tuple{T,T} where {N,T}
 	n = length(X)
 	intervalSize = tree.leafSize * 2^(length(tree.intervalSums)-d)
 
@@ -224,7 +223,7 @@ function bounds2(::Val{N}, ::Type{T}, d, k1, k2, α, X, tree::SumTree)::Tuple{T,
 end
 
 
-function ssumstep!(heap::BinaryMaxHeap{Block2}, ::Val{N}, ::Type{T},α,X,tree::SumTree)::Tuple{T,T} where {N,T}
+function ssumstep!(heap::BinaryMaxHeap{Block}, ::Val{N}, ::Type{T},α,X,tree::SumTree)::Tuple{T,T} where {N,T}
 	block = pop!(heap)
 
 	# remove existing bounds
@@ -241,28 +240,28 @@ function ssumstep!(heap::BinaryMaxHeap{Block2}, ::Val{N}, ::Type{T},α,X,tree::S
 		k2 = 2block.k2-1 # first child in second direction
 		nbrBlocks = length(tree.intervalSums[d])
 
-		lb11,ub11 = bounds2(Val{N}(), T, d, k1, k2, α, X, tree)
+		lb11,ub11 = bounds(Val{N}(), T, d, k1, k2, α, X, tree)
 		lb += lb11
 		ub += ub11
-		lb11!=ub11 && push!(heap, Block2(d, k1, k2, lb11, ub11))
+		lb11!=ub11 && push!(heap, Block(d, k1, k2, lb11, ub11))
 
 		if k1<k2<=nbrBlocks # only care about upper triangular part
-			lb21,ub21 = bounds2(Val{N}(), T, d, k1+1, k2, α, X, tree)
+			lb21,ub21 = bounds(Val{N}(), T, d, k1+1, k2, α, X, tree)
 			lb += lb21
 			ub += ub21
-			lb21!=ub21 && push!(heap, Block2(d, k1+1, k2, lb21, ub21))
+			lb21!=ub21 && push!(heap, Block(d, k1+1, k2, lb21, ub21))
 		end
 
 		if k2<nbrBlocks
-			lb12,ub12 = bounds2(Val{N}(), T, d, k1, k2+1, α, X, tree)
+			lb12,ub12 = bounds(Val{N}(), T, d, k1, k2+1, α, X, tree)
 			lb += lb12
 			ub += ub12
-			lb12!=ub12 && push!(heap, Block2(d, k1, k2+1, lb12, ub12))
+			lb12!=ub12 && push!(heap, Block(d, k1, k2+1, lb12, ub12))
 
-			lb22,ub22 = bounds2(Val{N}(), T, d, k1+1, k2+1, α, X, tree)
+			lb22,ub22 = bounds(Val{N}(), T, d, k1+1, k2+1, α, X, tree)
 			lb += lb22
 			ub += ub22
-			lb22!=ub22 && push!(heap, Block2(d, k1+1, k2+1, lb22, ub22))
+			lb22!=ub22 && push!(heap, Block(d, k1+1, k2+1, lb22, ub22))
 		end
 	end
 	lb,ub
@@ -272,9 +271,9 @@ end
 # Let s = ∑ᵢ∑ⱼϕ⁽ᴺ⁾(α⁻¹(Xᵢ-Xⱼ)).
 # returns lower and upper bounds for s, terminating when the tolerance is good enough
 function sumapprox(::Val{N},::Type{T},α,X,tree::SumTree;rtol)::Tuple{T,T} where {N,T}
-	heap = BinaryMaxHeap{Block2}()
-	lb,ub = bounds2(Val{N}(),T,1,1,1,α,X,tree)
-	push!(heap,Block2(1,1,1,lb,ub))
+	heap = BinaryMaxHeap{Block}()
+	lb,ub = bounds(Val{N}(),T,1,1,1,α,X,tree)
+	push!(heap,Block(1,1,1,lb,ub))
 	while !isempty(heap) && !isapprox(lb,ub;rtol=rtol)
 		lb,ub = (lb,ub) .+ ssumstep!(heap,Val{N}(),T,α,X,tree)
 	end
@@ -283,10 +282,10 @@ end
 
 # Let s = ∑ᵢ∑ⱼϕⁱᵛ(α⁻¹(Xᵢ-Xⱼ)).
 # returns sign(s-C)
-function ssign2(::Type{T},α,X,tree::SumTree,C)::Int where T
-	heap = BinaryMaxHeap{Block2}()
-	lb,ub = bounds2(Val{4}(),T,1,1,1,α,X,tree)
-	push!(heap,Block2(1,1,1,lb,ub))
+function ssign(::Type{T},α,X,tree::SumTree,C)::Int where T
+	heap = BinaryMaxHeap{Block}()
+	lb,ub = bounds(Val{4}(),T,1,1,1,α,X,tree)
+	push!(heap,Block(1,1,1,lb,ub))
 	while !isempty(heap) && lb<=C<=ub
 		lb,ub = (lb,ub) .+ ssumstep!(heap,Val{4}(),T,α,X,tree)
 	end
@@ -298,7 +297,7 @@ end
 
 
 # Computes the sign of the objective function by gradually approximating using lower/upper bounds
-function objectivesign2(h, X::AbstractArray{T}, tree::SumTree, α2Constant) where T
+function objectivesign(h, X::AbstractArray{T}, tree::SumTree, α2Constant) where T
 	n = length(X)
 	α2 = α2Constant*h^(5/7) # 1.357[SD(a)/TD(b)]^(1/7) * h^(5/7)
 
@@ -306,10 +305,10 @@ function objectivesign2(h, X::AbstractArray{T}, tree::SumTree, α2Constant) wher
 	C -= n*1.1968268412042982 # n*ϕ4(0) - get rid of the diagonal entries
 	C/=2 # because of symmetry, we can sum over j>i (upper triangular part), effectively halving the sum
 
-	ssign2(promote_type(Float64,T), α2, X, tree, C)
+	ssign(promote_type(Float64,T), α2, X, tree, C)
 end
 
-function SD_bounded2(α, X::AbstractArray{T}, tree::SumTree; rtol=0.05) where T
+function SD_bounded(α, X::AbstractArray{T}, tree::SumTree; rtol=0.05) where T
 	# α⁻⁵∑ᵢ∑ⱼϕⁱᵛ(α⁻¹(Xᵢ-Xⱼ))
 	n = length(X)
 	lb,ub = sumapprox(Val{4}(), promote_type(Float64,T), α, X, tree; rtol=rtol)
@@ -317,7 +316,7 @@ function SD_bounded2(α, X::AbstractArray{T}, tree::SumTree; rtol=0.05) where T
 	s/α^5
 end
 
-function TD_bounded2(b, X::AbstractArray{T}, tree::SumTree; rtol=0.05) where T
+function TD_bounded(b, X::AbstractArray{T}, tree::SumTree; rtol=0.05) where T
 	# b⁻⁷∑ᵢ∑ⱼϕᵛⁱ(b⁻¹(Xᵢ-Xⱼ))
 	n = length(X)
 	lb,ub = sumapprox(Val{6}(), promote_type(Float64,T), b, X, tree; rtol=rtol)
@@ -326,7 +325,7 @@ function TD_bounded2(b, X::AbstractArray{T}, tree::SumTree; rtol=0.05) where T
 end
 
 
-function _bwsj_bounded2(X; rtol=0.1)
+function _bwsj(X; rtol=0.1)
 	n = length(X)
 	tree = SumTree(X,10)
 
@@ -335,7 +334,7 @@ function _bwsj_bounded2(X; rtol=0.1)
 	a = 0.920λ*n^(-1/7)
 	b = 0.912λ*n^(-1/9)
 	# α2Constant = 1.357*(SD(a,X)/TD(b,X))^(1/7)
-	α2Constant = 1.357*(SD_bounded2(a,X,tree;rtol=0.5rtol)/TD_bounded2(b,X,tree;rtol=0.5rtol))^(1/7)
+	α2Constant = 1.357*(SD_bounded(a,X,tree;rtol=0.5rtol)/TD_bounded(b,X,tree;rtol=0.5rtol))^(1/7)
 
 
 	# Decide if we need to deal with bounds better.
@@ -345,208 +344,19 @@ function _bwsj_bounded2(X; rtol=0.1)
 	lower = λ*n^(-1/5)*1e-9
 	upper = λ*n^(-1/5)*1e9
 
-	find_zero(h->objectivesign2(h,X,tree,α2Constant), (lower,upper), Bisection(), xrtol=rtol)
+	find_zero(h->objectivesign(h,X,tree,α2Constant), (lower,upper), Bisection(), xrtol=rtol)
 end
 
 
-bwsj_bounded2(X; kwargs...) = _bwsj_bounded2(issorted(X) ? X : sort(X); kwargs...)
-
-
-
-
-
-#---------------------------------------------------------------------------------------------------
-# New algorithm v1
-
-
-struct Block
-	i1::Int
-	i2::Int
-	j1::Int
-	j2::Int
-	lb::Float64
-	ub::Float64
-end
-Base.isless(b1::Block,b2::Block) = isless(b1.ub-b1.lb, b2.ub-b2.lb)
-
-
-function bounds(::Type{T}, i1, i2, j1, j2, α, X)::Tuple{T,T} where T
-	if (i2-i1)<=20 || (j2-j1)<=20 # fallback - loop over the data points
-		# @info "Fallback for block $block"
-		s = zero(T)
-		if i1==j1
-			@inbounds for i in i1:i2
-				for j in max(i+1,j1):j2 # upper triangular part
-					s += ϕ4((X[i]-X[j])/α)
-				end
-			end
-		else
-			@inbounds for i in i1:i2
-				for j in j1:j2
-					s += ϕ4((X[i]-X[j])/α)
-				end
-			end
-		end
-		s,s # exact bounds
-	elseif i1==j1 # block on the diagonal
-		@assert i2==j2
-		npoints = div((i2-i1+1)*(i2-i1),2)
-		# npoints.*(-0.7399861849949221, 1.1968268412042982) # TODO: improve this
-		npoints.*ϕ4extrema(0.0, (X[i2]-X[i1])/α) # TODO: improve this.
-	else
-		npoints = (i2-i1+1)*(j2-j1+1)
-		npoints.*ϕ4extrema((X[j1]-X[i2])/α, (X[j2]-X[i1])/α)
-	end
-end
-
-
-
-# Let s = ∑ᵢ∑ⱼϕⁱᵛ(α⁻¹(Xᵢ-Xⱼ)).
-# returns sign(s-C)
-function ssign(::Type{T},α,X,C)::Int where T
-	n = length(X)
-	C -= n*1.1968268412042982 # n*ϕ4(0) - get rid of the diagonal entries
-	C/=2 # because of symmetry, we can sum over j>i (upper triangular part), effectively halving the sum
-
-	heap = BinaryMaxHeap{Block}()
-	lb,ub = bounds(T, 1, n, 1, n, α, X)
-	push!(heap,Block(1,n,1,n,lb,ub))
-
-	while true
-		lb > C && return 1
-		ub < C && return -1
-		isempty(heap) && break
-		block = pop!(heap)
-
-		# remove existing bounds
-		lb -= block.lb
-		ub -= block.ub
-
-		# create child blocks
-		midI = div(block.i1+block.i2,2)
-		midJ = div(block.j1+block.j2,2)
-
-		lb11,ub11 = bounds(T, block.i1, midI, block.j1, midJ, α, X)
-		lb += lb11
-		ub += ub11
-		lb11!=ub11 && push!(heap, Block(block.i1, midI, block.j1, midJ, lb11, ub11))
-
-		lb12,ub12 = bounds(T, block.i1, midI, midJ+1, block.j2, α, X)
-		lb += lb12
-		ub += ub12
-		lb12!=ub12 && push!(heap, Block(block.i1, midI, midJ+1, block.j2, lb12, ub12))
-
-		if block.j1>block.i1 # only care about upper triangular part
-			lb21,ub21 = bounds(T, midI+1, block.i2, block.j1, midJ, α, X)
-			lb += lb21
-			ub += ub21
-			lb21!=ub21 && push!(heap, Block(midI+1, block.i2, block.j1, midJ, lb21, ub21))
-		end
-
-		lb22,ub22 = bounds(T, midI+1, block.i2, midJ+1, block.j2, α, X)
-		lb += lb22
-		ub += ub22
-		lb22!=ub22 && push!(heap, Block(midI+1, block.i2, midJ+1, block.j2, lb22, ub22))
-	end
-
-	@warn "Numerical issues for upper/lower bounds, ub=$ub, lb=$lb, C=$C."
-	return 0
-end
-
-
-
-
-# Computes the sign of the objective function by gradually approximating using lower/upper bounds
-function objectivesign(h, X::AbstractArray{T}, α2Constant) where T
-	n = length(X)
-	α2 = α2Constant*h^(5/7) # 1.357[SD(a)/TD(b)]^(1/7) * h^(5/7)
-
-	C = (n-1)*α2Constant^5*h^(-10/7)/(2*√π)
-	ssign(promote_type(Float64,T), α2, X, C)
-end
-
-
-function _bwsj_bounded(X; rtol=0.1)
-	n = length(X)
-	q25,q75 = quantile(X,(0.25,0.75))
-	λ = min(q75-q25, std(X)*1.349) # Givivng the same scale estimate as e.g. the KernSmooth R package.
-	a = 0.920λ*n^(-1/7)
-	b = 0.912λ*n^(-1/9)
-	α2Constant = 1.357*(SD(a,X)/TD(b,X))^(1/7)
-
-	# Decide if we need to deal with bounds better.
-	# But note that execution is very fast for bad guesses for h.
-	lower = λ*n^(-1/5)*1e-9
-	upper = λ*n^(-1/5)*1e9
-
-	find_zero(h->objectivesign(h,X,α2Constant), (lower,upper), Bisection(), xrtol=rtol)
-end
-
-
-bwsj_bounded(X; kwargs...) = _bwsj_bounded(issorted(X) ? X : sort(X); kwargs...)
+bwsj(X; kwargs...) = _bwsj(issorted(X) ? X : sort(X); kwargs...)
 
 
 
 #---------------------------------------------------------------------------------------------------
 
 
-
-function SDrec(::Type{T},α,X,i1,i2,j1,j2)::T where T
-	j1<i1 && return zero(T) # early out, skip lower triangular part
-
-
-	if (i2-i1)<=5 || (j2-j1)<=5 # fallback - loop over the data points
-		s = zero(T)
-
-		if i1==j1
-			@inbounds for i in i1:i2
-				for j in max(i+1,j1):j2 # upper triangular part
-					s += ϕ4((X[i]-X[j])/α)
-				end
-			end
-		else
-			@inbounds for i in i1:i2
-				for j in j1:j2
-					s += ϕ4((X[i]-X[j])/α)
-				end
-			end
-		end
-		return s
-	end
-
-	if i1!=j1 # only consider approximation if it's not the same interval
-		npoints = (i2-i1+1)*(j2-j1+1)
-		lb,ub = npoints.*ϕ4extrema((X[j1]-X[i2])/α, (X[j2]-X[i1])/α) # proper bounds
-
-		if ub-lb < 1 # 20 # 0.1 # 0.01 # what is a good bound?
-			return (lb+ub)/2 # approximation
-		end
-	end
-
-	# split and recurse
-	midI = div(i1+i2,2)
-	midJ = div(j1+j2,2)
-	SDrec(T,α,X,i1,midI,j1,midJ) + SDrec(T,α,X,i1,midI,midJ+1,j2) + SDrec(T,α,X,midI+1,i2,j1,midJ) + SDrec(T,α,X,midI+1,i2,midJ+1,j2)
-end
-
-
-# assumes X is sorted
-function SDapprox(α, X::AbstractArray{T}) where T
-	# @show α
-	n = length(X)
-	s = 2*SDrec(promote_type(T,Float64),α,X,1,n,1,n)
-	s += n*ϕ4(0) # diagonal
-
-	# @info s
-	@assert s>0 "s>0 failed: $s"
-
-	s/(n*(n-1)*α^5)
-end
-
-
-
-# unoptimized version
-function SD(α, X::AbstractArray{T}) where T
+# Unoptimized version for reference
+function SD_reference(α, X::AbstractArray{T}) where T
 	# (n(n-1))⁻¹α⁻⁵∑ᵢ∑ⱼϕⁱᵛ(α⁻¹(Xᵢ-Xⱼ))
 	n = length(X)
 	C = 1/(n*(n-1)*α^5)
@@ -561,7 +371,7 @@ function SD(α, X::AbstractArray{T}) where T
 	C * s
 end
 
-function TD(b, X::AbstractArray{T}) where T
+function TD_reference(b, X::AbstractArray{T}) where T
 	# -(n(n-1))⁻¹b⁻⁷∑ᵢ∑ⱼϕᵛⁱ(b⁻¹(Xᵢ-Xⱼ))
 	n = length(X)
 	C = -1/(n*(n-1)*b^7)
@@ -577,7 +387,7 @@ function TD(b, X::AbstractArray{T}) where T
 end
 
 
-function objective(h, X, α2Constant)
+function objective_reference(h, X, α2Constant)
 	# [ R(K) / (nσ⁴ₖSD(α₂(h))) ]^(1/5) - h = 0
 	# R(K) = 1/(2√π)
 	# σ⁴ₖ = 1
@@ -585,18 +395,17 @@ function objective(h, X, α2Constant)
 	α2 = α2Constant*h^(5/7) # 1.357[SD(a)/TD(b)]^(1/7) * h^(5/7)
 	# denom = (n*2*√π) * SD(α2,X)
 	# denom^(-1/5) - h
-	1/(2*√(π)*n*h^5) - SD(α2,X)
+	1/(2*√(π)*n*h^5) - SD_reference(α2,X)
 end
 
 
-function _bwsj(X; rtol=0.1)
+function _bwsj_reference(X; rtol=0.1)
 	n = length(X)
 	q25,q75 = quantile(X,(0.25,0.75))
 	λ = min(q75-q25, std(X)*1.349) # Givivng the same scale estimate as e.g. the KernSmooth R package.
 	a = 0.920λ*n^(-1/7)
 	b = 0.912λ*n^(-1/9)
-	α2Constant = 1.357*(SD(a,X)/TD(b,X))^(1/7)
-
+	α2Constant = 1.357*(SD_reference(a,X)/TD_reference(b,X))^(1/7)
 
 	# Figure out lower and upper bounds for search.
 	# The Bandwidth scales roughly with n^(-1/5). TODO: Use this fact!
@@ -614,11 +423,9 @@ function _bwsj(X; rtol=0.1)
 		upper*=10
 	end
 
-	find_zero(h->objective(h,X,α2Constant), (lower,upper), Bisection(), xrtol=rtol)
+	find_zero(h->objective_reference(h,X,α2Constant), (lower,upper), Bisection(), xrtol=rtol)
 end
 
 
-# bwsj(X; kwargs...) = _bwsj(issorted(X) ? X : sort(X); kwargs...)
-bwsj_reference(X; kwargs...) = _bwsj(issorted(X) ? X : sort(X); kwargs...)
-bwsj(X; kwargs...) = _bwsj_bounded2(issorted(X) ? X : sort(X); kwargs...)
+bwsj_reference(X; kwargs...) = _bwsj_reference(issorted(X) ? X : sort(X); kwargs...)
 
