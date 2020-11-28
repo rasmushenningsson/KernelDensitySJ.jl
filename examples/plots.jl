f656f11dc36234547faae1d9d20e2f6a28b5c6f2
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2020-11-28 22:24:13 +0100

    Plotting fixes
    Better Gadfly usage.
    Add legends.
    Save plots to svg.

--- examples/plots.jl
+++ examples/plots.jl
@@ -1,38 +1,42 @@
 using KernelDensitySJ
 using Gadfly
 using Colors
+using DataFrames
 using StableRNGs
 
-set_default_plot_size(40cm, 30cm)
+theme = Theme(background_color=colorant"white")
 
 rng = StableRNG(123456)
 
 px = -1 .+ 2.0.*rand(rng, 40)
 py = cos.(π.*px) .+ 0.2.*randn(rng, length(px))
 
-x = range(-1.0,stop=1.0,length=1001)
+x = range(-1.0,stop=1.0,length=201)
 bandwidths = [0.03, 0.1, 0.3]
 yd = density(px, bandwidths', x)
 yd ./= maximum(yd;dims=1)
-
-colors = [LCHab{Float64}(70.0,60.0,240.0), LCHab{Float64}(80.0,70.000015,100.43479), LCHab{Float64}(65.89944,62.21457,353.99814)]
-
-layers = [layer(x=x,y=yd[:,i],color=[colors[i]],Geom.line) for i=1:length(bandwidths)]
-pl1 = plot(layer(x=px,y=zeros(size(px)),color=[colorant"black"],Geom.point), layers...)
-
 ys = smooth(px, py, bandwidths', x)
-layers = [layer(x=x,y=ys[:,i],color=[colors[i]],Geom.line) for i=1:length(bandwidths)]
-pl2 = plot(layer(x=px,y=py,color=[colorant"black"],Geom.point), layers...)
 
+df = DataFrame(x=repeat(x,3), bandwidth=repeat(string.(bandwidths),inner=length(x)), density=vec(yd), smoothed=vec(ys))
 
+pl1 = plot(layer(df, x=:x, y=:density, color=:bandwidth, Geom.line),
+           layer(x=px,y=0*py,color=[colorant"black"],Geom.point), theme)
+pl2 = plot(layer(df, x=:x, y=:smoothed, color=:bandwidth, Geom.line),
+           layer(x=px,y=py,color=[colorant"black"],Geom.point), theme)
 
 bwopt = bwsj(px)
-@show bwopt
 ydopt = density(px, bwopt, x)
 ydopt ./= maximum(ydopt)
-pl3 = plot(layer(x=px,y=zeros(size(px)),color=[colorant"black"],Geom.point), layer(x=x,y=ydopt,Geom.line))
-
 ysopt = smooth(px, py, bwopt, x)
-pl4 = plot(layer(x=px,y=py,color=[colorant"black"],Geom.point), layer(x=x,y=ysopt,Geom.line))
 
-gridstack([pl1 pl2; pl3 pl4])
+dfopt = DataFrame(x=x, bandwidth=string(round(bwopt,digits=3)), density=ydopt, smoothed=ysopt)
+
+pl3 = plot(layer(dfopt, x=:x, y=:density, color=:bandwidth, Geom.line),
+           layer(x=px,y=0*py,color=[colorant"black"],Geom.point), theme)
+pl4 = plot(layer(dfopt, x=:x, y=:smoothed, color=:bandwidth, Geom.line),
+           layer(x=px,y=py,color=[colorant"black"],Geom.point), theme)
+
+draw(SVG("pl1.svg", 16cm, 12cm), pl1)
+draw(SVG("pl2.svg", 16cm, 12cm), pl2)
+draw(SVG("pl3.svg", 16cm, 12cm), pl3)
+draw(SVG("pl4.svg", 16cm, 12cm), pl4)
