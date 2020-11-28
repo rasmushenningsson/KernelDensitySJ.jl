5ad6f81c7e8c9a3834beff7b35313958298a22ff
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2020-11-28 20:39:02 +0100

    Updated plot example

--- examples/plots.jl
+++ examples/plots.jl
@@ -1,30 +1,38 @@
 using KernelDensitySJ
 using Gadfly
 using Colors
+using StableRNGs
 
-px = rand(10)
-py = Float64.(px.>0.5)
+set_default_plot_size(30cm, 30cm)
 
-x = range(-0.5,stop=1.5,length=201)
-bandwidths = [0.02, 0.06, 0.2]
+rng = StableRNG(123456)
+
+px = -1 .+ 2.0.*rand(rng, 60)
+py = cos.(π.*px) .+ 0.2.*randn(rng, length(px))
+
+x = range(-1.0,stop=1.0,length=1001)
+bandwidths = [0.3, 0.1, 0.03]
 yd = density(px, bandwidths', x)
 yd ./= maximum(yd;dims=1)
 
 colors = [colorant"blue",colorant"orange",colorant"purple"]
 
 layers = [layer(x=x,y=yd[:,i],color=[colors[i]],Geom.line) for i=1:length(bandwidths)]
-display(plot(layer(x=px,y=zeros(size(px)),color=[colorant"black"],Geom.point), layers...))
+pl1 = plot(layer(x=px,y=zeros(size(px)),color=[colorant"black"],Geom.point), layers...)
 
 ys = smooth(px, py, bandwidths', x)
 layers = [layer(x=x,y=ys[:,i],color=[colors[i]],Geom.line) for i=1:length(bandwidths)]
-display(plot(layer(x=px,y=py,color=[colorant"black"],Geom.point), layers...))
+pl2 = plot(layer(x=px,y=py,color=[colorant"black"],Geom.point), layers...)
 
 
 
 bwopt = bwsj(px)
+@show bwopt
 ydopt = density(px, bwopt, x)
 ydopt ./= maximum(ydopt)
-display(plot(layer(x=px,y=zeros(size(px)),color=[colorant"black"],Geom.point), layer(x=x,y=ydopt,Geom.line)))
+pl3 = plot(layer(x=px,y=zeros(size(px)),color=[colorant"black"],Geom.point), layer(x=x,y=ydopt,Geom.line))
 
 ysopt = smooth(px, py, bwopt, x)
-display(plot(layer(x=px,y=py,color=[colorant"black"],Geom.point), layer(x=x,y=ysopt,Geom.line)))
+pl4 = plot(layer(x=px,y=py,color=[colorant"black"],Geom.point), layer(x=x,y=ysopt,Geom.line))
+
+gridstack([pl1 pl2; pl3 pl4])
