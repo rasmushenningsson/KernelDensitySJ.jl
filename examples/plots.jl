587a7e4aee45879c127d26291318f30a11e78aa0
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2020-11-28 20:59:55 +0100

    Minor example changes

--- examples/plots.jl
+++ examples/plots.jl
@@ -3,19 +3,19 @@ using Gadfly
 using Colors
 using StableRNGs
 
-set_default_plot_size(30cm, 30cm)
+set_default_plot_size(40cm, 30cm)
 
 rng = StableRNG(123456)
 
-px = -1 .+ 2.0.*rand(rng, 60)
+px = -1 .+ 2.0.*rand(rng, 40)
 py = cos.(π.*px) .+ 0.2.*randn(rng, length(px))
 
 x = range(-1.0,stop=1.0,length=1001)
-bandwidths = [0.3, 0.1, 0.03]
+bandwidths = [0.03, 0.1, 0.3]
 yd = density(px, bandwidths', x)
 yd ./= maximum(yd;dims=1)
 
-colors = [colorant"blue",colorant"orange",colorant"purple"]
+colors = [LCHab{Float64}(70.0,60.0,240.0), LCHab{Float64}(80.0,70.000015,100.43479), LCHab{Float64}(65.89944,62.21457,353.99814)]
 
 layers = [layer(x=x,y=yd[:,i],color=[colors[i]],Geom.line) for i=1:length(bandwidths)]
 pl1 = plot(layer(x=px,y=zeros(size(px)),color=[colorant"black"],Geom.point), layers...)
